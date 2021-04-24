de0463af703a23c8ee0c230481acd91133cc5699
young-Howard <81988494+young-Howard@users.noreply.github.com>
2021-04-24 13:29:40 -0700

    Update Howard_Bot.py

--- Howard_Bot.py
+++ Howard_Bot.py
@@ -1,10 +1,9 @@
-
 # A id 829086651379875889
 # public key 5776288308f096645768b39539c99285b34d98f7d8af1b6fbc8b8cf06f795a80
 # Token ODI5MDg2NjUxMzc5ODc1ODg5.YGzBHQ.fbimYqbXqV3X5NWUIAQGLLOuKxw
 # perms int 75840
 # https://discordapp.com/oauth2/authorize?client_id=829086651379875889&scope=bot&permissions=75840
-
+from discord.ext import commands
 import random
 import discord
 
@@ -13,16 +12,15 @@ client = discord.Client()
 
 @client.event
 async def on_ready():
-    await client.change_presence(activity=discord.Activity(type=discord.ActivityType.watching, name='your mother'))
+    await client.change_presence(activity=discord.Activity(type=discord.ActivityType.watching, name='out for your commands!'))
     print(f"We have logged in as {client.user}")
 @client.event
 async def on_message(message):
     print (f"{message.channel}:{message.author}:{message.author.name}:{message.content}")
-    #await client.change_presence(activity=discord.Game('your mom'))
 
 
     if "_help" in message.content.lower():
-        await message.channel.send("-note that must be behind all of the commands in order for them to be noticed by Howard_bot\n-leave will disconnect the bot")
+        await message.channel.send("-note that must be behind all of the commands in order for them to be noticed by Howard_bot\n-leave, will disconnect the bot\n-play, will be a 1-2 guessing game")
 
     if "retard" in message.content.lower():
         await message.channel.send("not nice :(")
@@ -31,8 +29,30 @@ async def on_message(message):
         await message.channel.send("bye!!")
         await client.close()
 
-    if str(message.author) == "vsvs" and "" in message.content.lower():
+    if str(message.author) == "" and "" in message.content.lower():
         await message.channel.send('')
+
+    if message.content.startswith("_play"): #the play command to start the guessing game.
+        channel = message.channel
+        await channel.send("Choose a number between 1-2. Enter numerical values only.") #message that tells about the start of the game
+
+ 
+
+        # generates a random number and turns it into a string
+    number1 = random.randint(1,2)        
+    number2 = str(number1)
+
+
+    def check(m):
+        return m.content == number2 and m.channel == channel 
+        """
+          The check function, first it checks if the message is the correct number.
+          Then it checks if the channel is the same channel as the channel that the play command was sent in.
+          If both is true, then it returns true. Else it returns false.
+        """
+        
+    msg = await client.wait_for('message', check=check) #waits for the check function to return true
+    await channel.send("Correct answer {.author}" .format(msg)) #sends the correct answer message
 
 
 client.run("ODI5MDg2NjUxMzc5ODc1ODg5.YGzBHQ.fbimYqbXqV3X5NWUIAQGLLOuKxw")
