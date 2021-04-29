27825096d62aed16635cec70652555bbdccc1822
young-Howard <81988494+young-Howard@users.noreply.github.com>
2021-04-28 21:06:39 -0700

    Update Howard_Bot.py

--- Howard_Bot.py
+++ Howard_Bot.py
@@ -1,9 +1,3 @@
-# A id 829086651379875889
-# public key 5776288308f096645768b39539c99285b34d98f7d8af1b6fbc8b8cf06f795a80
-# Token ODI5MDg2NjUxMzc5ODc1ODg5.YGzBHQ.fbimYqbXqV3X5NWUIAQGLLOuKxw
-# perms int 75840
-# https://discordapp.com/oauth2/authorize?client_id=829086651379875889&scope=bot&permissions=75840
-
 from discord.ext import commands
 import random
 import discord
@@ -21,7 +15,7 @@ async def on_message(message):
 
 
     if "_help" in message.content.lower():
-        await message.channel.send("-note that must be behind all of the commands in order for them to be noticed by Howard_bot\n-leave, will disconnect the bot\n-play, will be a 1-2 guessing game")
+        await message.channel.send("-note _ that must be behind all of the commands in order for them to be noticed by Howard_bot\n-leave, will disconnect the bot\n-play, will be a 1-2 guessing game")
 
     if "retard" in message.content.lower():
         await message.channel.send("not nice :(")
@@ -30,7 +24,7 @@ async def on_message(message):
         await message.channel.send("bye!!")
         await client.close()
 
-    if str(message.author) == "Young_Howard#5726" and "" in message.content.lower():
+    if str(message.author) == "" and "" in message.content.lower():
         await message.channel.send('Hi howard!')
 
     if message.content.startswith("_play"): 
@@ -45,7 +39,14 @@ async def on_message(message):
         return m.content == number2 and m.channel == channel 
         
     msg = await client.wait_for('message', check=check) 
-    await channel.send("Correct answer {.author}" .format(msg)) 
+    await channel.send("Correct answer {.author}" .format(msg))
+
+    def check(m):
+        return m.content == number2 and m.channel == channel 
+
+    msg2 != await client.wait_for('message', check=False) #waits for the check function to return true
+    await channel.send("wrong {.author}" .format(msg2)) #sends the correct answer message
 
 
-client.run("ODI5MDg2NjUxMzc5ODc1ODg5.YGzBHQ.fbimYqbXqV3X5NWUIAQGLLOuKxw")
+
+client.run("")
